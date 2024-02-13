b029ca8df21758f7ebe79705363cd760d070c43a
Jonas Lindberg <eksno@protonmail.com>
2024-02-13 12:43:32 +0000

    setup autogen with fastapi and return chatresult

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from typing import Union
-
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
+
+load_dotenv()
+openai_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 
@@ -10,6 +14,14 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/run")
+def run_autogen():
+    config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+    assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
+    user_proxy = UserProxyAgent(
+        "user_proxy", code_execution_config={"work_dir": "coding"}
+    )
+    chat_result = user_proxy.initiate_chat(
+        assistant, message="Plot a chart of NVDA and TESLA stock price change YTD."
+    )
+    return {"chat_result": chat_result}
