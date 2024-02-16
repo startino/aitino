9f153dd2df54553633f7f6c09aeaaa66b2c0f9e8
Jonas Lindberg <eksno@protonmail.com>
2024-02-16 15:20:47 +0000

    supabase and updating packages

--- aitino/__init__.py
+++ aitino/__init__.py
@@ -6,13 +6,8 @@ from .maeve import Maeve
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/run")
-def run_autogen():
+@app.get("/v0.0.0/run")
+def run_autogen(maeve_id: str):
     maeve = Maeve("gpt-4-turbo-preview", composition=composition)
 
     maeve.run("Plan and improve the github repository https://github.com/Futino/futino")
--- shell.nix
+++ shell.nix
@@ -2,8 +2,7 @@
 (pkgs.buildFHSUserEnv {
     name = "pipzone";
     targetPkgs = pkgs: (with pkgs; [
-        python311
-        python311Packages.pip
+        python312
         pipx
         poetry
     ]);
