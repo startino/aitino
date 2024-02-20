f5d2c2c188a1ad121069abf8262f72919cd86d5f
Leon Nilsson <leonlamnilsson@gmail.com>
2024-02-20 09:19:33 +0100

    changed 1 word so improver might be better at limiting its wordcount

--- aitino/improver.py
+++ aitino/improver.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 def improve_prompt(word_limit: int, prompt: str) -> str:
     with open(Path(os.getcwd(), "aitino", "prompts", "improve-prompt.md"), "r", encoding="utf-8") as f:
         system_prompt = f.read()
-    system_prompt += f"\n4. Limit the amount of words in the prompt to {word_limit} words"
+    system_prompt += f"\n4. Limit the amount of words in this prompt to {word_limit} words"
     
     client = OpenAI()
     result = client.chat.completions.create(
