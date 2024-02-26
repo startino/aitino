14767fbcb5f8685710a543d1ef19a85ef2418d03
Jonas Lindberg <eksno@protonmail.com>
2024-02-26 07:33:16 +0000

    create model files

--- aitino/models/__init__.py
+++ aitino/models/__init__.py
@@ -2,10 +2,14 @@ from .agent_config import AgentConfig
 from .api_reply import StreamReply
 from .code_execution_config import CodeExecutionConfig
 from .llm_config import LLMConfig
+from .message import Message
+from .session import Session
 
 __all__ = [
     "AgentConfig",
     "StreamReply",
     "CodeExecutionConfig",
     "LLMConfig",
+    "Message",
+    "Session",
 ]
