d991b967179a577462f852e7184909402014d4fd
Jonas Lindberg <eksno@protonmail.com>
2024-02-28 04:12:59 +0100

    aiti.no cors

--- aitino/__init__.py
+++ aitino/__init__.py
@@ -37,6 +37,7 @@ app.add_middleware(
         "http://127.0.0.1:8001",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:8081",
+        "https://aiti.no",
     ],
     allow_credentials=True,
     allow_methods=["*"],
