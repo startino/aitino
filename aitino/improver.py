3934bc7b19c4cc942243b048b883bc675624d2cf
Leon Lam Nilsson <leonlamnilsson@gmail.com>
2024-02-28 13:13:45 +0100

    add mocking for improve prompt unit tests

--- aitino/improver.py
+++ aitino/improver.py
@@ -1,16 +1,38 @@
 import os
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Protocol
 
 from openai import OpenAI
 
-
 PromptType = Literal["generic", "system", "user"]
 
+class InvalidPromptTypeError(BaseException):
+    ...
+class ContentProtocol(Protocol):
+    content: str
+
+class MessageProtocol(Protocol):
+    message: ContentProtocol
+
+class ResponseProtocol(Protocol):
+    choices: list[MessageProtocol]
+    
+class CompletionsProtocol(Protocol):
+    def create(self, messages, model, temperature, frequency_penalty, presence_penalty) -> ResponseProtocol:
+        ...
 
 def improve_prompt(
-    word_limit: int, prompt: str, prompt_type: PromptType, temperature: float = 0.0
-) -> str:
+    word_limit: int, prompt: str, prompt_type: PromptType, client: CompletionsProtocol, temperature: float = 0.0
+) -> str | None:
+    if (word_limit <= 0):
+        raise ValueError("Word limit must be greater than 0")
+
+    if (prompt_type not in ["generic", "system", "user"]):
+        raise InvalidPromptTypeError(f"Invalid prompt type: {prompt_type}")
+    
+    if (temperature < -2.0 or temperature > 2.0):
+        raise ValueError("Temperature must be in between -2 and 2")
+
     with open(
         Path(os.getcwd(), "aitino", "prompts", "improve", prompt_type + ".md"),
         "r",
@@ -21,8 +43,7 @@ def improve_prompt(
             f"\n4. Limit the amount of words in this prompt to {word_limit} words"
         )
 
-    client = OpenAI()
-    result = client.chat.completions.create(
+    result = client.create(
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": prompt},
@@ -34,4 +55,4 @@ def improve_prompt(
     )
 
     content = result.choices[0].message.content
-    return content if content else "error"
+    return content if content else None
--- aitino/tests/test_thing.py
+++ aitino/tests/test_thing.py
@@ -1,6 +1,63 @@
 import pytest
 
-from ..improver import improve_prompt
+from ..improver import improve_prompt, InvalidPromptTypeError
+from unittest.mock import patch
 
+class MockContent:
+    content: str
+
+class MockMessage:
+    message: MockContent
+
+class MockResponse:
+    def __init__(self):
+        self.choices: list[MockMessage] = []
+
+class MockCompletions():
+    def create(self, messages, model, temperature, frequency_penalty, presence_penalty) -> MockResponse:
+        response = MockResponse()
+        message = MockMessage()
+        content = MockContent()
+
+        content.content = "yes sir"
+        message.message = content
+        response.choices.append(message)
+
+        return response
+
+class MockFailingCompletions():
+    def create(self, messages, model, temperature, frequency_penalty, presence_penalty) -> MockResponse:
+        response = MockResponse()
+        message = MockMessage()
+        content = MockContent()
+
+        content.content = ""
+        message.message = content
+        response.choices.append(message)
+
+        return response
+
+def test_mock_failing_completions():
+    assert not improve_prompt(100, "here's a test prompt", "generic", MockFailingCompletions(), 0.0)
+
+def test_improve_prompt_word_limit():
+    with pytest.raises(ValueError) as error: 
+        improve_prompt(0, "here's a test prompt", "generic", MockCompletions(), 0.0)
+
+    assert error
+
+def test_improve_prompt_type():
+    with pytest.raises(InvalidPromptTypeError) as error:
+        improve_prompt(100, "here's a test prompt", "nottype", MockCompletions(), 0.0)
+    
+    assert error
+
+def test_improve_prompt_temperature():
+    with pytest.raises(ValueError):
+        improve_prompt(100, "here's a test prompt", "generic", MockCompletions(), -2.1)
+    
+    with pytest.raises(ValueError):
+        improve_prompt(100, "here's a test prompt", "generic", MockCompletions(), 2.1)
+    
 def test_improve_prompt():
-    assert improve_prompt(100, "here's a test prompt haha", "generic", 0.0)  != "error"
+    assert improve_prompt(100, "here's a test prompt", "generic", MockCompletions(), 0.0)
