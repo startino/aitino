749ab103ab15f1c567f2a6279a5f1702f0102ca1
Jonas Lindberg <eksno@protonmail.com>
2024-03-01 04:52:57 +0000

    cors allow *

--- aitino/__init__.py
+++ aitino/__init__.py
@@ -23,21 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:8000",
-        "http://localhost:8001",
-        "http://localhost:8080",
-        "http://localhost:8081",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://127.0.0.1:8000",
-        "http://127.0.0.1:8001",
-        "http://127.0.0.1:8080",
-        "http://127.0.0.1:8081",
-        "https://aiti.no",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
