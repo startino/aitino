dfc7fc9b8c7fb9d0ed7349ca59e2add920debfc8
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-03-04 01:45:26 +0800

    updated bg color of sidenav and hover animation of navelements

--- apps/web/src/lib/components/ui/side-nav/side-nav.svelte
+++ apps/web/src/lib/components/ui/side-nav/side-nav.svelte
@@ -46,7 +46,7 @@
 <div class="hidden h-full py-8 pl-4 lg:z-50 lg:flex lg:w-72 lg:flex-col{$$props.class}">
 	<!-- Sidebar component, swap this element with another sidebar if you like -->
 	<div
-		class="bg-card flex grow flex-col gap-y-5 overflow-hidden rounded-2xl border px-6 pb-6 text-white"
+		class="bg-primary-950/30 flex grow flex-col gap-y-5 overflow-hidden rounded-2xl border px-6 pb-6 text-white"
 	>
 		<div class="flex h-16 shrink-0 items-center px-2 pt-6">
 			<a href="/app/auto-build" class="mr-4 flex place-items-center space-x-2">
@@ -71,7 +71,7 @@
 											href
 										)
 											? 'bg-accent/90 text-accent-foreground hover:bg-accent '
-											: 'text-foreground hover:text-accent opacity-100'}"
+											: 'text-foreground hover:text-accent hover:bg-primary/5 opacity-100'}"
 									>
 										<svelte:component this={icon} />
 										{name}
@@ -98,7 +98,7 @@
 										href
 									)
 										? 'bg-accent/90 text-foreground hover:bg-accent '
-										: 'text-foreground hover:text-accent opacity-100'}"
+										: 'text-foreground hover:text-accent hover:bg-primary/5 opacity-100'}"
 								>
 									<svelte:component this={icon} />
 									{name}
