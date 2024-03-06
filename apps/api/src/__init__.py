dfce5d8e5c6940c0f39257492df2b85878e3ec8f
Leon Lam Nilsson <leonlamnilsson@gmail.com>
2024-03-06 19:57:22 +0100

    test fixing things

--- apps/api/src/__init__.py
+++ apps/api/src/__init__.py
@@ -14,9 +14,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse, StreamingResponse
 from openai import OpenAI
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
 
 
 from .improver import PromptType, improve_prompt
