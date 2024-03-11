d4e5a3cd0a5002ebd353f8b1dfbc377f2c1d5261
Jonas Lindberg <eksno@protonmail.com>
2024-03-11 16:06:20 +0000

    format and fixup sh file

--- apps/api/src/__init__.py
+++ apps/api/src/__init__.py
@@ -10,7 +10,6 @@ from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
-
 from . import mock as mocks
 from .autobuilder import build_agents
 from .crew import Crew
--- apps/api/src/models/crew_model.py
+++ apps/api/src/models/crew_model.py
@@ -1,4 +1,5 @@
 from uuid import UUID
+
 from pydantic import BaseModel
 
 from .agent import Agent
@@ -6,6 +7,10 @@ from .agent import Agent
 
 class CrewModel(BaseModel):
     reciever_id: UUID
-    delegator_id: UUID | None = None  # None means admin again, so its the original crew (has no parent crew)
+    delegator_id: UUID | None = (
+        None  # None means admin again, so its the original crew (has no parent crew)
+    )
     agents: list[Agent]
-    sub_crews: list['CrewModel'] = []  # Must set delegator_id for each sub_crew in sub_crews
+    sub_crews: list["CrewModel"] = (
+        []
+    )  # Must set delegator_id for each sub_crew in sub_crews
