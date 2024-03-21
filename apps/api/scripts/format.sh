73a013a785a4e0b1d3d9dd813301a11f3f88ee81
Leon Lam Nilsson <leonlamnilsson@gmail.com>
2024-03-21 18:25:32 +0100

    run format script with poetry

--- apps/api/scripts/format.sh
+++ apps/api/scripts/format.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --force-single-line-imports .
-autoflake --remove-all-unused-imports --remove-unused-variables --in-place . --exclude=__init__.py
-black .
-isort .
+poetry run isort --force-single-line-imports .
+poetry run autoflake --remove-all-unused-imports --remove-unused-variables --in-place . --exclude=__init__.py
+poetry run black .
+poetry run isort .
