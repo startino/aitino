5447368d6ae2dc5a9e85a049ecad57bcbb1077d2
Leon Nilsson <70595163+failandimprove1@users.noreply.github.com>
2024-03-28 10:31:45 +0100

    Update test_thing.sh
    Signed-off-by: Leon Nilsson <70595163+failandimprove1@users.noreply.github.com>

--- apps/api/scripts/test_thing.sh
+++ apps/api/scripts/test_thing.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-ENDPOINT="http://localhost:8000/openapi.json"
+ENDPOINT="https://api.aiti.no/openapi.json"
 
 response=$(curl -s -o /dev/null -w "%{http_code}" $ENDPOINT)
 
 echo $response
 if [ $response -lt 400 ]; then
-    java -jar openapi-generator-cli.jar generate -i $ENDPOINT -g javascript -o api_client
+    java -jar openapi-generator-cli.jar generate -i $ENDPOINT -g typescript -o api_client
     echo "all good"
 else
     echo "The openapi schema is not reachable, are you sure the server is running?"
