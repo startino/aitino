04e541a906eb0d6c0e891023e6a9a5b6b00dd249
Leon Lam Nilsson <leonlamnilsson@gmail.com>
2024-04-08 14:59:04 +0200

    add google search tool wip
    gotta add the optional wrapper parameters aswell

--- apps/api/src/mock.py
+++ apps/api/src/mock.py
@@ -81,7 +81,7 @@ move_file: dict = {
     "created_at": "2024-01-01T00:00:00.000Z",
 }
 
-tool, prompt = "duckduckgo search tool", "search for information about Google Gemini with region set to the UK"
+tool, prompt = "google serper tool", "search for information about Google Gemini"
 
 test_tool: dict = {
     "id": "00000000-0000-0000-0000-000000000001",
