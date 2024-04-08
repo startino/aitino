fbdb0bbe4f5ee4abff318cca37fc8c8e79b1f8fd
Leon Nilsson <70595163+failandimprove1@users.noreply.github.com>
2024-04-08 11:27:23 +0200

    fix response type of api key get request (#162)
    also added updating a key from the keys id

--- apps/api/src/models/api_key.py
+++ apps/api/src/models/api_key.py
@@ -7,9 +7,18 @@ class APIKeyRequestModel(BaseModel):
     api_key_type_id: UUID
     api_key: str
 
+class APIKeyTypeModel(BaseModel):
+    id: UUID
+    name: str
+    created_at: datetime
+    description: str
+
 class APIKeyResponseModel(BaseModel):
     id: UUID
     created_at: datetime
     profile_id: UUID
-    api_key_type_id: UUID
+    api_key_type: APIKeyTypeModel | None = None
+    api_key: str
+
+class APIKeyUpdateModel(BaseModel):
     api_key: str
