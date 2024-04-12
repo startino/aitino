389af54cbf8511c72c2e8c7132d4e703b2d7794f
Leon Lam Nilsson <leonlamnilsson@gmail.com>
2024-04-12 11:50:12 +0200

    remove auth things

--- apps/api/src/routers/auth.py
+++ apps/api/src/routers/auth.py
@@ -29,37 +29,6 @@ if url is None or key is None:
 logger = logging.getLogger("root")
 
 
-@router.post("/sign_up")
-def email_sign_up(
-    sign_up_request: SignUpWithEmailAndPasswordCredentials,
-) -> AuthResponse:
-    """format for passing display name: 'options': {'data':{'display_name': 'name'}}"""
-    supabase: Client = create_client(url, key)
-    try:
-        response =supabase.auth.sign_up(sign_up_request)
-
-    except errors.AuthApiError as e:
-        raise HTTPException(e.status, e.to_dict())
-
-    user = response.user
-    if user and user.user_metadata.get("display_name"):
-        display_name = user.user_metadata["display_name"]
-        supabase.table("profiles").update({"display_name": display_name}).eq("id", user.id).execute()
-
-    return response
-
-@router.post("/sign_in")
-def email_sign_in(
-    sign_in_request: SignInWithEmailAndPasswordCredentials,
-) -> AuthResponse:
-    supabase: Client = create_client(url, key)
-    try:
-        return supabase.auth.sign_in_with_password(sign_in_request)
-
-    except errors.AuthApiError as e:
-        raise HTTPException(e.status, e.to_dict())
-
-
 @router.post("/sign_in/provider")
 def provider_sign_in(provider_request: SignInWithOAuthCredentials) -> OAuthResponse:
     supabase: Client = create_client(url, key)
