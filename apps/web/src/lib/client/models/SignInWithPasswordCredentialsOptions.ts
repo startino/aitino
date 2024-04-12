0c13847171f51115c27c1ccce638681ea4e2b99f
Jonas Lindberg <eksno@protonmail.com>
2024-04-12 07:32:56 +0200

    remove client

--- apps/web/src/lib/client/models/SignInWithPasswordCredentialsOptions.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type SignInWithPasswordCredentialsOptions = {
-    data?: any;
-    captcha_token?: string;
-};
-
