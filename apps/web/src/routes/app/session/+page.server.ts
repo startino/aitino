b58b616597fd287f452ce33f8b10de00e5ccab5b
Jonas Lindberg <eksno@protonmail.com>
2024-04-12 11:42:01 +0200

    api things

--- apps/web/src/routes/app/session/+page.server.ts
+++ apps/web/src/routes/app/session/+page.server.ts
@@ -1,36 +1,38 @@
-import {
-	CrewsService,
-	SessionsService,
-	type CrewResponseModel,
-	type SessionResponse
-} from '$lib/client';
 import { error, redirect } from '@sveltejs/kit';
+import api from '$lib/api';
 
 export const load = async ({ url, locals: { getSession } }) => {
 	const userSession = await getSession();
 
 	if (!userSession) throw error(401, 'You are not logged in. Please log in and try again.');
 
-	const sessions: SessionResponse[] = await SessionsService.getSessionsSessionsGet(
-		userSession.user.id,
-		null
-	).catch((e: unknown) => {
-		console.error(`Error retrieving sessions: ${e}`);
-		return [];
+	const sessions = await api.GET('/sessions/', {
+		params: {
+			query: {
+				by_profile: userSession.user.id
+			}
+		}
 	});
+	// const sessions: SessionResponse[] = await SessionsService.getSessionsSessionsGet(
+	// 	userSession.user.id,
+	// 	null
+	// ).catch((e: unknown) => {
+	// 	console.error(`Error retrieving sessions: ${e}`);
+	// 	return [];
+	// });
 
 	if (sessions.length > 0 && !url.searchParams.has('debug')) {
 		console.log(`Redirecting to session ${sessions[0].id}`);
 		redirect(303, `/app/session/${sessions[0].id}`);
 	}
 
-	const crews: CrewResponseModel[] = await CrewsService.getCrewsOfUserCrewsGet(
-		userSession.user.id,
-		false
-	).catch((e: unknown) => {
-		console.error(`Error retrieving crews: ${e}`);
-		return [];
-	});
+	// const crews: CrewResponseModel[] = await CrewsService.getCrewsOfUserCrewsGet(
+	// 	userSession.user.id,
+	// 	false
+	// ).catch((e: unknown) => {
+	// 	console.error(`Error retrieving crews: ${e}`);
+	// 	return [];
+	// });
 
 	const data = {
 		profileId: userSession.user.id,
