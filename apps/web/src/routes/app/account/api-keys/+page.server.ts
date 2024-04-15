e7cc44d27ce3f939bd209d165312b97c9e9d1765
Jonas Lindberg <eksno@protonmail.com>
2024-04-15 20:51:43 +0200

    partially convert api keys to new system

--- apps/web/src/routes/app/account/api-keys/+page.server.ts
+++ apps/web/src/routes/app/account/api-keys/+page.server.ts
@@ -1,41 +1,91 @@
-import { fail } from '@sveltejs/kit';
+import { error, fail } from '@sveltejs/kit';
 import { message, setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import api from '$lib/api';
 
 import { apiKeySchema } from '$lib/schema';
 
-// TODO: Implement api client instead of these legacy ones
+export const load = async ({ locals: { getSession } }) => {
+	const userSession = await getSession();
+	if (!userSession) throw error(401, 'You are not logged in. Please log in and try again.');
 
-export const load = async ({ locals }) => {
+	const userApiKeys = await api
+		.GET('/profiles/{profile_id}/api_keys', {
+			params: {
+				path: {
+					profile_id: userSession.user.id
+				}
+			}
+		})
+		.then(({ data: d, error: e }) => {
+			if (e) {
+				console.error(`Error retrieving api keys: ${e}`);
+				return [];
+			}
+			if (!d) {
+				console.error(`No data returned from api keys`);
+				return [];
+			}
+			return d;
+		});
+
+	const apiKeyTypes = await api.GET('/api_key_types/').then(({ data: d, error: e }) => {
+		if (e) {
+			console.error(`Error retrieving api key types: ${e}`);
+			return [];
+		}
+		if (!d) {
+			console.error(`No data returned from api key types`);
+			return [];
+		}
+		return d;
+	});
+
 	const form = await superValidate(zod(apiKeySchema));
-	const session = await locals.getSession();
-
-	const userApiKeys = await ProfilesService.getApiKeysProfilesProfileIdApiKeysGet(
-		session?.user.id as string
-	);
-
-	const apiKeyTypes = await ApiKeyTypesService.getAllApiKeyTypesApiKeyTypesGet();
-
 	return { form, apiKeyTypes, userApiKeys };
 };
 
 export const actions = {
 	create: async ({ request, locals }) => {
+		const userSession = await locals.getSession();
+		if (!userSession) throw error(401, 'You are not logged in. Please log in and try again.');
+
 		const form = await superValidate(request, zod(apiKeySchema));
 
 		if (!form.valid) {
 			return fail(400, { form });
 		}
 
-		const session = await locals.getSession();
+		// TODO: Tomorrow
+		// const data = await api
+		// 	.PATCH('/profiles/api_keys/{api_key_id}', {
+		// 		params: {
+		// 			path: {
+		// 				api_key_id: form.data.value
+		// 			},
+		// 		},
+		//               body: {
+		//                   api_key: form.data.value,
+		//               }
+		// 	})
+		// 	.then(({ data: d, error: e }) => {
+		// 		if (e) {
+		// 			console.error(`Error creating api key: ${e}`);
+		// 			return [];
+		// 		}
+		// 		if (!d) {
+		// 			console.error(`No data returned from api key creation`);
+		// 			return [];
+		// 		}
+		// 	});
 
-		const data = await ProfilesService.insertApiKeyProfilesApiKeysPost({
-			profile_id: session?.user.id as string,
-			api_key: form.data.value,
-			api_key_type_id: form.data.typeId
-		}).catch((e) => {
-			return setError(form, 'Something went wrong', { status: 500 });
-		});
+		// const data = await ProfilesService.insertApiKeyProfilesApiKeysPost({
+		// 	profile_id: session?.user.id as string,
+		// 	api_key: form.data.value,
+		// 	api_key_type_id: form.data.typeId
+		// }).catch((e) => {
+		// 	return setError(form, 'Something went wrong', { status: 500 });
+		// });
 
 		return message(form, 'API Key added!');
 	},
@@ -47,10 +97,10 @@ export const actions = {
 			return fail(400, { message: 'No id provided' });
 		}
 
-		await ProfilesService.deleteApiKeyProfilesApiKeysApiKeyIdDelete(id).catch(() => {
-			fail(500);
-		});
-
+		// await ProfilesService.deleteApiKeyProfilesApiKeysApiKeyIdDelete(id).catch(() => {
+		// 	fail(500);
+		// });
+		//
 		return { message: 'API Key deleted successfully' };
 	}
 };
