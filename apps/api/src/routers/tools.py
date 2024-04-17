0a4feedb2f237b695fdc2690e60aa321d0f12492
Leon Nilsson <70595163+failandimprove1@users.noreply.github.com>
2024-04-17 17:19:33 +0200

    Add tool endpoints (#201)
    * add tools (untested)
    
    * change api key types endpoint name to kebab case
    
    * add error handling on delete tool
    
    ---------
    
    Signed-off-by: Leon Nilsson <70595163+failandimprove1@users.noreply.github.com>

--- apps/api/src/routers/tools.py
+++ apps/api/src/routers/tools.py
@@ -5,16 +5,54 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from src.interfaces import db
 from src.models import (
-    AgentInsertRequest,
-    AgentUpdateModel,
     Agent,
-    AgentGetRequest,
+    Tool,
+    ToolGetRequest,
+    ToolInsertRequest,
+    ToolUpdateRequest,
 )
 
 router = APIRouter(
     prefix="/tools",
     tags=["tools"],
 )
+
+@router.get("/")
+def get_tools(q: ToolGetRequest = Depends()) -> list[Tool]:
+    return db.get_tools(q.name, q.api_key_type_id)
+
+
+@router.get("/{tool_id}")
+def get_tool(tool_id: UUID) -> Tool:
+    response = db.get_tool(tool_id)
+    if not response:
+        raise HTTPException(404, "tool not found")
+    
+    return response
+
+
+@router.post("/", status_code=201)
+def insert_tool(tool: ToolInsertRequest) -> Tool:
+    return db.insert_tool(tool)
+
+
+@router.delete("/{tool_id}")
+def delete_tool(tool_id: UUID) -> Tool:
+    response = db.delete_tool(tool_id)
+    if not response:
+        raise HTTPException(404, "could not find tool")
+    
+    return response
+
+
+@router.patch("/{tool_id}")
+def update_profile(
+    tool_id: UUID, tool_update_request: ToolUpdateRequest
+) -> Tool:
+    if not db.get_tool(tool_id):
+        raise HTTPException(404, "tool not found")
+
+    return db.update_tool(tool_id, tool_update_request)
 
 
 @router.patch("/{agent_id}")
