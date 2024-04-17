36990211e55dee1982e2b4034d698787f74af10b
Leon Nilsson <70595163+failandimprove1@users.noreply.github.com>
2024-04-17 11:24:55 +0200

    Fix the types of session and message models for the openapi code generator (#197)
    * fix the type on session
    
    * fix type on message
    
    * fix warning from edge class
    
    * clean up, also fix message model

--- apps/api/src/models/message.py
+++ apps/api/src/models/message.py
@@ -5,14 +5,14 @@ from pydantic import BaseModel, Field
 
 
 class Message(BaseModel):
-    id: UUID = Field(default_factory=lambda: uuid4())
+    id: UUID 
     session_id: UUID
     profile_id: UUID
     sender_id: UUID | None = None  # None means admin here
     recipient_id: UUID | None = None  # None means admin here aswell
     content: str
-    role: str = "user"
-    created_at: datetime = Field(default_factory=lambda: datetime.now(tz=UTC))
+    role: str 
+    created_at: datetime
 
 
 class MessageInsertRequest(BaseModel):
