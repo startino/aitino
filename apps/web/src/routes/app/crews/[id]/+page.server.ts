a76b72d83271e7ac5172637d9430f5d2649f451f
Jonas Lindberg <eksno@protonmail.com>
2024-04-21 20:19:58 +0200

    it's erroring because it's trying to add the agent twice somewhere I think

--- apps/web/src/routes/app/crews/[id]/+page.server.ts
+++ apps/web/src/routes/app/crews/[id]/+page.server.ts
@@ -15,31 +15,40 @@ const getNodesByCrewId = async (crew: schemas['Crew']): Promise<Node[]> => {
 		.then(async ({ data: d, error: e }) => {
 			if (e) {
 				console.error(`Failed to load agents for crew ${crew}. ${e.detail}. Using HEAVY fallback`);
-				let a = [];
-				// for (let agentId of crew.nodes) {
-				// 	a.push(
-				// 		await api
-				// 			.GET('/agents/{id}', {
-				// 				params: {
-				// 					path: {
-				// 						id: agentId
-				// 					}
-				// 				}
-				// 			})
-				// 			.then(({ data: d2, error: e2 }) => {
-				// 				if (e2) {
-				// 					console.error(`Failed to load agent ${agentId}. ${e2.detail}`);
-				// 					return;
-				// 				}
-				// 				if (!d2) {
-				// 					console.error(`No data returned from agent ${agentId}`);
-				// 					return;
-				// 				}
-				// 				return d2;
-				// 			})
-				// 	);
-				// }
-				// return a;
+				let a: schemas['Agent'][] = [];
+				for (let agentId of crew.nodes) {
+					console.log(`Using fallback for agent ${agentId}`);
+					const ag = await api
+						.GET('/agents/{id}', {
+							params: {
+								path: {
+									id: agentId
+								}
+							}
+						})
+						.then(({ data: d2, error: e2 }) => {
+							if (e2) {
+								console.error(`Failed to load agent ${agentId}. ${e2.detail}`);
+								return;
+							}
+							if (!d2) {
+								console.error(`No data returned from agent ${agentId}`);
+								return;
+							}
+							return d2;
+						});
+					if (!ag) {
+						console.error(`Skipping null agent ${agentId}`);
+						continue;
+					}
+					if (a.find((a: schemas['Agent']) => a.id === ag.id)) {
+						console.error(`Skipping duplicate agent ${ag.id}`);
+						continue;
+					}
+					a = [...a, ag];
+				}
+				console.log(JSON.stringify(a));
+				return a;
 			}
 			if (!d) {
 				console.error(`No data returned from agents`);
@@ -51,12 +60,6 @@ const getNodesByCrewId = async (crew: schemas['Crew']): Promise<Node[]> => {
 	let nodes: Node[] = [];
 
 	for (let agent of agents) {
-		if (!agent) {
-			console.error(
-				`Skipping null agent. This happened because the fallback above was used and one of the agents still failed to be retrieved.`
-			);
-			continue;
-		}
 		nodes.push({
 			id: agent.id,
 			type: 'agent',
@@ -146,7 +149,7 @@ export const load = async ({ locals: { getSession }, params }) => {
 		throw error(500, 'Failed to load published agents');
 	}
 
-	const nodes = getWritablePrompt(await getNodesByCrewId(crew.id));
+	const nodes = getWritablePrompt(await getNodesByCrewId(crew));
 
 	return {
 		profileId: userSession.user.id,
