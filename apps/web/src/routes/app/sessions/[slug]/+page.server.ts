16bb1b815ba526b3163bebffc95bf585ea376bf3
Jonas Lindberg <jonas.lindberg@futi.no>
2024-04-23 10:46:28 +0200

    loading sessions working (#227)

--- apps/web/src/routes/app/sessions/[slug]/+page.server.ts
+++ apps/web/src/routes/app/sessions/[slug]/+page.server.ts
@@ -5,10 +5,10 @@ export const load = async ({ params, locals: { getSession } }) => {
 	const userSession = await getSession();
 
 	const session = await api
-		.GET('/sessions/{session_id}', {
+		.GET('/sessions/{id}', {
 			params: {
 				path: {
-					session_id: params.slug
+					id: params.slug
 				}
 			}
 		})
@@ -24,6 +24,26 @@ export const load = async ({ params, locals: { getSession } }) => {
 			return d;
 		});
 
+	const sessions = await api
+		.GET('/sessions/', {
+			params: {
+				query: {
+					profile_id: userSession.user.id
+				}
+			}
+		})
+		.then(({ data: d, error: e }) => {
+			if (e) {
+				console.error(`Error retrieving sessions: ${e.detail}`);
+				return [];
+			}
+			if (!d) {
+				console.error(`No data returned from sessions`);
+				return [];
+			}
+			return d;
+		});
+
 	// TODO: Handle crew not found better.
 	// If the crew is not found, then it is very likely that the user
 	// managed to delete their crew while the crew still had sessions. This
@@ -31,10 +51,10 @@ export const load = async ({ params, locals: { getSession } }) => {
 	// we will want to completely prevent deletion of crews with active sessions
 	// or use some sort of cascade delete behind very strong confirmation dialogs.
 	const crew = await api
-		.GET('/crews/{crew_id}', {
+		.GET('/crews/{id}', {
 			params: {
 				path: {
-					crew_id: session.crew_id
+					id: session.crew_id
 				}
 			}
 		})
@@ -56,67 +76,65 @@ export const load = async ({ params, locals: { getSession } }) => {
 			return d;
 		});
 
+	const crews = await api
+		.GET('/crews/', {
+			params: {
+				query: {
+					profile_id: userSession.user.id
+				}
+			}
+		})
+		.then(({ data: d, error: e }) => {
+			if (e) {
+				console.error(`Error retrieving crews: ${e.detail}`);
+				return [];
+			}
+			if (!d) {
+				console.error(`No data returned from crews`);
+				return [];
+			}
+			return d;
+		});
+
+	const messages = await api
+		.GET('/messages/', {
+			params: {
+				query: {
+					session_id: session.id
+				}
+			}
+		})
+		.then(({ data: d, error: e }) => {
+			if (e) {
+				console.error(`Error retrieving messages: ${e.detail}`);
+				return [];
+			}
+			if (!d) {
+				console.error(`No data returned from messages`);
+				return [];
+			}
+			return d;
+		});
+
+	const agents = await api.GET('/agents/').then(({ data: d, error: e }) => {
+		if (e) {
+			console.error(`Error retrieving agents: ${e.detail}`);
+			return [];
+		}
+		if (!d) {
+			console.error(`No data returned from agents`);
+			return [];
+		}
+		return d;
+	});
+
 	return {
 		profileId: userSession.user.id,
 		session: session,
-		sessions: await api
-			.GET('/sessions/', {
-				params: {
-					query: {
-						profile_id: userSession.user.id
-					}
-				}
-			})
-			.then(({ data: d, error: e }) => {
-				if (e) {
-					console.error(`Error retrieving sessions: ${e.detail}`);
-					return [];
-				}
-				if (!d) {
-					console.error(`No data returned from sessions`);
-					return [];
-				}
-				return d;
-			}),
+		sessions: sessions,
 		crew: crew,
-		crews: await api
-			.GET('/crews/', {
-				params: {
-					query: {
-						profile_id: userSession.user.id
-					}
-				}
-			})
-			.then(({ data: d, error: e }) => {
-				if (e) {
-					console.error(`Error retrieving crews: ${e.detail}`);
-					return [];
-				}
-				if (!d) {
-					console.error(`No data returned from crews`);
-					return [];
-				}
-				return d;
-			}),
-		messages: [],
-		agents: await api
-			.GET('/agents/', {
-				params: {
-					query: {
-						crew_id: userSession.user.id
-					}
-				}
-			})
-			.then(({ data: d, error: e }) => {
-				if (e) {
-					console.error(`Error retrieving agents: ${e.detail}`);
-					return [];
-				}
-				if (!d) {
-					console.error(`No data returned from agents`);
-					return [];
-				}
-				return d;
-			})
+		crews: crews,
+		messages: messages,
+		agents: agents
 	};
 };
