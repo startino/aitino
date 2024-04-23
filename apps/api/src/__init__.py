739f91a76d3c02e8c5dbea6bd4c1003f6cfc004f
Leon Nilsson <leonlamnilsson@gmail.com>
2024-04-23 14:54:45 +0200

    change port number on server

--- apps/api/src/__init__.py
+++ apps/api/src/__init__.py
@@ -83,5 +83,5 @@ def create_app() -> FastAPI:
 
 if __name__ == "__main__":
     app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_config="logging.yaml")
+    uvicorn.run(app, host="0.0.0.0", port=80, log_config="logging.yaml")
 
