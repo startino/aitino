7a2521629551898fdce593ec5bf6999c2f2aab2b
Jonas Lindberg <jonas.lindberg@futi.no>
2024-04-23 10:46:28 +0200

    loading sessions working (#227)

--- apps/web/src/routes/app/sessions/[slug]/+page.svelte
+++ apps/web/src/routes/app/sessions/[slug]/+page.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
+	import { getContext, setContext } from '$lib/utils';
+	import { writable } from 'svelte/store';
 	import Chat from './Chat.svelte';
 	import SessionNavigator from './SessionNavigator.svelte';
 	import { Loader2 } from 'lucide-svelte';
+	import type { SessionContext } from '$lib/types';
 
 	export let data;
 
-	let profileId = data.profileId;
-	let crew = data.crew;
-	let crews = data.crews;
-	let session = data.session;
-	let sessions = data.sessions;
-	let messages = data.messages;
-	let agents = data.agents;
+	let writableData: SessionContext = {
+		profileId: writable(data.profileId),
+		session: writable(data.session),
+		sessions: writable(data.sessions),
+		crew: writable(data.crew),
+		crews: writable(data.crews),
+		messages: writable(data.messages),
+		agents: writable(data.agents)
+	};
+
+	setContext('session', writableData);
+	let { session, sessions, crew } = getContext('session');
 </script>
 
-<h1 class="fixed top-4 pl-4 text-2xl font-bold">{session.title} - {crew.title}</h1>
+<h1 class="fixed top-4 pl-4 text-2xl font-bold">{$session.title} - {$crew.title}</h1>
 <div class="flex h-full flex-row place-items-center">
 	<div class="flex h-full w-full">
-		<Chat {session} {messages} {agents} />
+		<Chat />
 	</div>
 	{#if sessions}
-		<SessionNavigator {profileId} {sessions} {crews} {session} />
+		<SessionNavigator />
 	{:else}
 		<Loader2 />
 	{/if}
