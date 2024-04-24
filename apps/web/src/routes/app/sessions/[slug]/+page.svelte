d5a14ec842debe63da6d5b8b18398b1bc750190b
Jonas Lindberg <eksno@protonmail.com>
2024-04-24 19:18:30 +0200

    supabase subscribe

--- apps/web/src/routes/app/sessions/[slug]/+page.svelte
+++ apps/web/src/routes/app/sessions/[slug]/+page.svelte
@@ -6,6 +6,8 @@
 	import type { SessionContext } from '$lib/types';
 	import { Textarea } from '$lib/components/ui/textarea';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import { createClient } from '@supabase/supabase-js';
+	import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 	export let data;
 
@@ -21,6 +23,20 @@
 
 	setContext('session', writableData);
 	let { session, sessions, crew } = getContext('session');
+
+	// Initialize the JS client
+	const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+
+	// Create a function to handle inserts
+	const handleInserts = (payload) => {
+		console.log('Change received!', payload);
+	};
+
+	// Listen to inserts
+	supabase
+		.channel('todos')
+		.on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'todos' }, handleInserts)
+		.subscribe();
 </script>
 
 <h1 class="fixed left-[13em] right-0 top-0 z-10 w-full bg-background p-2 text-2xl font-bold">
