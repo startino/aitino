56f8b92448026fd9a83418de4ff75eba8f59c7d6
Jonas Lindberg <eksno@protonmail.com>
2024-04-24 20:30:36 +0200

    realtime and replies somewhat working

--- apps/web/src/routes/app/sessions/[slug]/+page.svelte
+++ apps/web/src/routes/app/sessions/[slug]/+page.svelte
@@ -8,6 +8,9 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { createClient } from '@supabase/supabase-js';
 	import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+	import api from '$lib/api';
+	import { toast } from 'svelte-sonner';
+	import { onMount } from 'svelte';
 
 	export let data;
 
@@ -22,31 +25,66 @@
 	};
 
 	setContext('session', writableData);
-	let { session, sessions, crew } = getContext('session');
+	let { session, messages, crew } = getContext('session');
 
-	// Initialize the JS client
+	const continueSession = async () => {
+		if ($session.reply !== '') {
+			const newSession = await api
+				.POST('/sessions/run', {
+					body: {
+						crew_id: $session.crew_id,
+						profile_id: $session.profile_id,
+						session_title: $session.title,
+						session_id: $session.id,
+						reply: $session.reply
+					}
+				})
+				.then(({ data: d, error: e }) => {
+					if (e) {
+						toast(`Error continueing session: ${e.detail}`);
+						return null;
+					}
+					if (!d) {
+						toast(`Error continueing session. No session was returned.`);
+						return null;
+					}
+					return d;
+				});
+
+			if (!newSession) return;
+
+			$session = newSession;
+			$session.reply = '';
+		}
+	};
+
+	// Initialize the TS client
 	const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
 	// Create a function to handle inserts
-	const handleInserts = (payload) => {
-		console.log('Change received!', payload);
+	const handleInserts = (payload: any) => {
+		if (payload.new.session_id === $session.id) {
+			$messages = [...$messages, payload.new];
+			console.log('New message:', payload.new);
+			window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+		}
 	};
 
 	// Listen to inserts
 	supabase
-		.channel('todos')
-		.on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'todos' }, handleInserts)
+		.channel('messages')
+		.on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, handleInserts)
 		.subscribe();
 </script>
 
 <h1 class="fixed left-[13em] right-0 top-0 z-10 w-full bg-background p-2 text-2xl font-bold">
 	{$session.title} - {$crew.title}
 </h1>
-<div class="flex flex-col px-12 pt-12">
+<div class="flex flex-col overflow-hidden px-12 pt-12">
 	<Chat />
 	<div class="my-8 flex h-full w-full items-center justify-center gap-2">
 		<Textarea bind:value={$session.reply}></Textarea>
-		<Button>Send</Button>
+		<Button on:click={continueSession}>Send</Button>
 	</div>
 </div>
 <SessionNavigator />
--- apps/web/src/routes/app/sessions/[slug]/Chat.svelte
+++ apps/web/src/routes/app/sessions/[slug]/Chat.svelte
@@ -5,14 +5,14 @@
 	import { Separator } from '$lib/components/ui/separator';
 
 	let { messages, agents } = getContext('session');
+
+	$messages.sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime());
 </script>
 
 {#each $messages as message, index}
-	{#if index !== 0}
-		<MessageItem {message} agents={$agents} />
+	<MessageItem {message} agents={$agents} />
 
-		{#if index !== $messages.length - 1}
-			<Separator class="my-12 scale-95 opacity-50" />
-		{/if}
+	{#if index !== $messages.length - 1}
+		<Separator class="my-12 scale-95 opacity-50" />
 	{/if}
 {/each}
