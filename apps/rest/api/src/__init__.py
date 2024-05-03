ff36f7934c45abde1c5abdddf666e5855468cb4c
Jonas Lindberg <eksno@protonmail.com>
2024-05-03 08:13:07 +0000

    whops forgor router

--- apps/rest/api/src/__init__.py
+++ apps/rest/api/src/__init__.py
@@ -2,10 +2,12 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.routers import index
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    app.include_router(index.router)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
