f5702453938340f583072ea2c91cc84e812086c6
Leon Nilsson <leonlamnilsson@gmail.com>
2024-05-08 16:58:03 +0000

    fix: Remove imports 2: electric boogaloo

--- apps/rest/api/src/__init__.py
+++ apps/rest/api/src/__init__.py
@@ -1,5 +1,4 @@
 import os
-import uvicorn
 from dotenv import load_dotenv
 import logging
 import diskcache as dc
@@ -17,7 +16,7 @@ from .models import (
     GenerateCommentRequest,
     FalseLead,
 )
-from src.reddit_worker import RedditStreamWorker
+from reddit_worker import RedditStreamWorker
 
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
